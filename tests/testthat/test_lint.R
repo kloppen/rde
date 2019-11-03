f469ed737ca1f87c43bd7ea1ca755c60735100c6
Stefan Kloppenborg <stefan@kloppenborg.ca>
2019-11-03 17:51:42 -0500

    Updated lintr test
    A recent change to the lintr package seems to
    cause the lintr test to fail on TravisCI and
    when launched from RStudio's "Check" function.
    This change addresses this.

--- tests/testthat/test_lint.R
+++ tests/testthat/test_lint.R
@@ -3,10 +3,31 @@ if (requireNamespace("lintr", quietly = TRUE)) {
 
   context("linting package")
   test_that("Package Style", {
-    expect_lint_free(
-      linters = with_defaults(
-        object_name_linter(styles = c("dotted.case", "snake_case"))
+
+    # There is currently a problem with the `lintr` package that causes
+    # `expect_lint_free` to fail when checking  a package from the RStudio
+    # IDE. For now, including the following check will skip the
+    # `expect_lint_free` when it will fail due to this problem
+
+    find_package <- function(path) {
+      path <- normalizePath(path, mustWork = FALSE)
+
+      while (!file.exists(file.path(path, "DESCRIPTION"))) {
+        path <- dirname(path)
+        if (identical(path, dirname(path))) {
+          return(NULL)
+        }
+      }
+
+      path
+    }
+
+    if (!is.null(find_package("."))) {
+      expect_lint_free(
+        linters = with_defaults(
+          object_name_linter(styles = c("dotted.case", "snake_case"))
+        )
       )
-    )
+    }
   })
 }
